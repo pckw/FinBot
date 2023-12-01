0ec139adc63ec5bb6feec1d6cd47a3147ed7eda5
Patrick Wagner <patrick.wagner@ppi.de>
2023-12-01 17:21:56 +0100

    Parameterise prompt templates

--- src/models/chatGPT.py
+++ src/models/chatGPT.py
@@ -1,13 +1,11 @@
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.llms import OpenAI
 from dotenv import load_dotenv
-from langchain.prompts import (
-    ChatPromptTemplate,
-    HumanMessagePromptTemplate,
-    SystemMessagePromptTemplate,
-)
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.prompts import PromptTemplate
 
 load_dotenv('.env')
+
 
 class chatGPT_assistant():
     def __init__(self, vectordb, model_name='gpt-3.5-turbo', temperature=0.7, k=6) -> None:
@@ -17,26 +15,42 @@ class chatGPT_assistant():
         self.vectordb = vectordb
     
     def query(self, query, chat_history=[]):
+        # open prompt templates
+        with open("prompt_templates/few_shot_doc_prompt_en_short.txt") as f:
+            template_few_shot_doc = f.read()
 
-        system_message_prompt = SystemMessagePromptTemplate.from_template(
-            """Your name is FinBot and you are an expert on financial data and reports.
-              Answer the user question based on the provided context delimited 
-              by tripple backticks. Make your answer as brief and short as 
-              possible. Answer with a single word or number when possible. 
-              If you don't know the answer, say that you don't know. 
-              Dont make up any answer.\n
-              ``` Context: {context} ```""") 
-        human_message_prompt = HumanMessagePromptTemplate.from_template(
-            "{question}")
+        with open("prompt_templates/summary_en.txt") as f:
+            template_summary = f.read()
+        # build prompts
+        SUMMARY_PROMPT = PromptTemplate.from_template(template_summary)
 
-        qa_chain = ConversationalRetrievalChain.from_llm(
-            ChatOpenAI(temperature=self.temperature,
-                        model_name=self.model_name),
-            retriever=self.vectordb.as_retriever(search_kwargs={'k': self.k}),
-            return_source_documents=True,
-            verbose=True,
-            combine_docs_chain_kwargs={"prompt": ChatPromptTemplate.
-                                       from_messages([system_message_prompt,
-                                                      human_message_prompt])},
-        )
-        return qa_chain({"question": query, "chat_history": chat_history})
+        QA_PROMPT = PromptTemplate(template=template_few_shot_doc,
+                                   input_variables=["summaries", "question"])
+        DOC_PROMPT = PromptTemplate(
+            template="Content: {page_content}\nSource: {source}",
+            input_variables=["page_content", "source"])
+
+        # Define llms
+        llm_for_chat = OpenAI(temperature=self.temperature,
+                              model=self.model_name)
+        llm_for_doc_chain = OpenAI(temperature=0)
+
+        # Define chains
+        question_generator = LLMChain(llm=llm_for_chat,
+                                      prompt=SUMMARY_PROMPT)
+        doc_chain = load_qa_with_sources_chain(llm_for_doc_chain,
+                                               chain_type="stuff",
+                                               verbose=True,
+                                               prompt=QA_PROMPT,
+                                               document_prompt=DOC_PROMPT)
+
+        chain = ConversationalRetrievalChain(
+                retriever=self.vectordb.as_retriever(search_kwargs={'k': self.k}),
+                question_generator=question_generator,
+                combine_docs_chain=doc_chain,
+                return_source_documents=True,
+                return_generated_question=True,
+                verbose=True
+            )
+        chat_history = []
+        return chain({"question": query, "chat_history": chat_history})
